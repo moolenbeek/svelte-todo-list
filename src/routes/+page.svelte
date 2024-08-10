2e74794995d4f49dca6ab2acef158f1b3ca20672
Eric - Wall Sound <emoolenbeek@gmail.com>
2024-08-10 08:40:09 -0400

    import and display TodoList

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
+<script lang="ts">
+	import TodoList from "$lib/todoList.svelte";
+</script>
+
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
 		<h1>Todo List</h1>
+		<TodoList />
 	</div>
 </div>
