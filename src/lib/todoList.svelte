e79f7fc63ca711d23c2881a789884bde3e446038
Eric - Wall Sound <emoolenbeek@gmail.com>
2024-08-10 12:42:17 -0400

    add on click to button and move array to todo.ts

--- src/lib/todoList.svelte
+++ src/lib/todoList.svelte
@@ -1,15 +1,5 @@
 <script lang="ts">
-	const listItems: ListItem[] = [
-		{
-			text: 'Banana'
-		},
-		{
-			text: 'Oat Milk'
-		},
-		{
-			text: 'Veggie Broth'
-		}
-	];
+  import { listItems, addItem, removeSelf } from "../todo";
 </script>
 
 <ol class="list">
@@ -17,12 +7,12 @@
 		<li>
 			<span>{i + 1}</span>
 			<input class="input" type="text" placeholder="Input" value={ListItem.text} disabled />
-			<button type="button" class="btn variant-ghost-error">x</button>
+			<button type="button" class="btn variant-ghost-error" on:click={() => removeSelf()}>x</button>
 		</li>
 	{/each}
 	<li>
 		<span>{listItems.length + 1}</span>
 		<input class="input" type="text" placeholder="Input" />
-		<button type="button" class="btn variant-ghost-primary">+</button>
+		<button type="button" class="btn variant-ghost-primary" on:click={() => addItem()}>+</button>
 	</li>
 </ol>
