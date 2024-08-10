6da32c1ed5a9930aca31e6ff393bc731cc97f08f
Eric - Wall Sound <emoolenbeek@gmail.com>
2024-08-10 12:43:00 -0400

    add listItems array and helper functions

---
+++ src/todo.ts
@@ -0,0 +1,21 @@
+export const listItems: ListItem[] = [
+		{
+			text: 'Banana'
+		},
+		{
+			text: 'Oat Milk'
+		},
+		{
+			text: 'Veggie Broth'
+		}
+];
+
+// add item to list
+export const addItem = () => {
+	console.log("add");
+};
+
+// remove self from list
+export const removeSelf = () => {
+	console.log("remove ");
+};
