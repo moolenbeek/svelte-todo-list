82ebf7ff24aaa6b6fe7922a21e201c4fb1375eac
Eric - Wall Sound <emoolenbeek@gmail.com>
2024-08-10 08:41:23 -0400

    add list item array and some html

---
+++ src/lib/todoList.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  const listItems: ListItem[] = [
+  {
+    text: "Banana"
+  },
+  {
+    text: "Oat Milk"
+  },
+  {
+    text: "Veggie Broth"
+  }
+  ]
+</script>
+
+<ol class="list">
+  {#each listItems as ListItem, i (i)}
+  <li>
+		<span>{i + 1}</span>
+    <input class="input" type="text" placeholder="Input" value={ListItem.text}>
+    <button type="button" class="btn variant-filled">x</button>
+	</li>
+  {/each}
+	<li>
+		<span>{listItems.length + 1}</span>
+    <input class="input" type="text" placeholder="Input" />
+    <button type="button" class="btn variant-filled">+</button>
+	</li>
+</ol>
